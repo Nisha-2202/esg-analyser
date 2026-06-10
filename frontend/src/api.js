37028536f4742201a6b4f86aa9d349d33a4e511d
Nisha-2202 <nishagovintharaj8438@gmail.com>
2026-06-10 11:03:47 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://esg-analyser-backend.onrender.com',
   headers: { 'Cache-Control': 'no-cache' }
 });
 API.interceptors.request.use(config => {
